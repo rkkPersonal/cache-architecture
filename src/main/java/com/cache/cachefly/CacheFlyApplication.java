4ea4918a8e3f10b39b3a3c4977e9172fb4b76760
rkkPersonal <17635841699@163.com>
2022-11-21 01:25:11 +0800

    first commit

---
+++ src/main/java/com/cache/cachefly/CacheFlyApplication.java
@@ -0,0 +1,13 @@
+package com.cache.cachefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheFlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheFlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cache/cachefly/CacheFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.cachefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheFlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
